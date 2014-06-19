e9792073ae71a786e8bc275a70f1a775a6443813
Kohki Miura <kohki@kohki-no-MacBook-Air.local>
2014-06-20 05:17:12 +0900

    Initial Commit

---
+++ Kotory 3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Kotory 3
+//
+//  Created by Kohki Miura on 2014/06/20.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
